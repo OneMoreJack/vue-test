4b4edef56d5c61431ea1fce9affd9758448d3055
JackLiR8 <jiakeli1024@gmail.com>
2020-02-27 11:45:14 +0800

    :construction: Greeting.vue demo

---
+++ src/components/Greeting.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ greeting }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+
+  data() {
+    return {
+      greeting: 'Hello, Unit Test'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
