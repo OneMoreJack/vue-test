7ce18a04743a41e92f450b8cc10d278aee8abace
JackLiR8 <jiakeli1024@gmail.com>
2020-02-27 15:23:31 +0800

    :construction: test props

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -51,3 +51,25 @@ describe('SubmitButton', () => {
     })
   })
 })
+
+/* 
+另一种写法，如果只涉及 renders a message 的测试时，推荐此中写法
+但若涉及到基于有无权限的其他测试，上一中写法更利于扩展
+
+describe('SubmitButton.vue', () => {
+  describe('renders a message', () => {
+    it('has admin privileges', () => {
+      const wrapper = factory()
+
+      expect(wrapper.find('span').text()).toBe('Not Authorized')
+      expect(wrapper.find('button').text()).toBe('submit')
+    })
+
+    it('does not have admin privileges', () => {
+      const wrapper = factory({ isAdmin: true })
+
+      expect(wrapper.find('span').text()).toBe('Admin Privileges')
+      expect(wrapper.find('button').text()).toBe('submit')
+    })
+  })
+}) */
