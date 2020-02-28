52890bd04ba37a1e532301821a6191212ab81c4e
JackLiR8 <jiakeli1024@gmail.com>
2020-02-28 23:55:59 +0800

    :sparkles: 模仿用户输入，防ajax请求

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -1,7 +1,42 @@
 import { shallowMount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 import FormSubmitter from '@/components/FormSubmitter'
 
+let url = ''
+let data = ''
+
+const mockHttp = {
+  get: (_url, _data) => {
+    return new Promise((resolve, reject) => {
+      url = _url
+      data = _data
+      resolve()
+    })
+  }
+}
+
 describe('FormSubmitter.vue', () => {
+  it('renders a notification when submitted successful', async () => {
+    const wrapper = shallowMount(FormSubmitter, {
+      mocks: {
+        $http: mockHttp
+      }
+    })
+
+    wrapper.find('[data-username]').setValue('Jack')
+    wrapper.find('form').trigger('submit.prevent')
+
+    // 确保包括nextTick在内的所有promises都被resolve，并且Vue也会更新DOM。
+    await flushPromises()
+
+    expect(wrapper.find('.message').text())
+      .toBe('Thank you for your submission, Jack.')
+    expect(url).toBe('/api/v1/register')
+    expect(data).toEqual({ username: 'Jack' })
+  })
+})
+
+/* describe('FormSubmitter.vue', () => {
   it('reveals a notification when submitted', async () => {
     // Arrange
     const wrapper = shallowMount(FormSubmitter)
@@ -15,4 +50,4 @@ describe('FormSubmitter.vue', () => {
     expect(wrapper.find('.message').text())
       .toBe('Thank you for your submission, Jack.')
   })
-})
+}) */
